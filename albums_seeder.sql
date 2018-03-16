b4b0d901ad047d2d41d6be44e14c5e5b9dee4ec9
Chris Allen <chrishughallen@gmail.com>
2018-03-16 16:11:42 -0500

    finished half of db selector exercise

---
+++ albums_seeder.sql
@@ -0,0 +1,40 @@
+USE codeup_test_db,
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Michael Jackson', 'Thriller', 1982, 47.3, 'pop,rock,r&b'),
+VALUES('AC/DC', 'Back in Black', 1980, 26.1, 'Hard Rock'),
+VALUES('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.2, 'Progressive Rock'),
+VALUES('Meat Loaf', 'Bat Out of Hell', 1977, 21.5, 'Hard Rock, Progressive Rock'),
+VALUES('Whitney Houston', 'The Bodyguard', 1992, 28.4, 'R&B, Soul, Pop, Soundtrack'),
+VALUES('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 32.2, 'Rock, Soft Rock, Folk Rock'),
+VALUES('Bee Gees', 'Saturday Night Fever', 1977, 21.6, 'Disco'),
+VALUES('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft Rock'),
+VALUES ('Various Artists', 'Grease: The original soundtrack from the motion picture', 1978, 29.0, 'Soundtrack'),
+VALUES('Led Zeppelin', 'Led Zeppelin IV', 1971, 29.0, 'Hard Rock, Heavy metal, folk'),
+VALUES('Michael Jackson', 'Bad', 1987, 19.3, 'Pop, Funk, Rock'),
+VALUES('Alanis Morissette', 'Jagged Little Pill', 1995, 24.4, 'Alternative Rock'),
+VALUES('Shania Twain', 'Come On Over', 1997, 29.6, 'Country, Pop'),
+VALUES('Celine Dion', 'Falling Into You', 1996, 20.2, 'Pop, Soft Rock'),
+VALUES('The Beatles', 'Sgt. Pepper\'s Lonely Hearts Club Band' , 1967, 13.1, 'Rock'),
+VALUES('Eagles', 'Hotel California', 1976, 21.5, 'Rock, Soft Rock, Folk Rock'),
+VALUES('Various Artists', 'Dirty Dancing', 1987, 17.9, 'Pop, R&B, Rock'),
+VALUES('Adele', '21', 2011, 25.3, 'Pop, Soul'),
+VALUES('Celine Dion', 'Let\'s Talk About Love', 1997, 19.3, 'Pop, Soft Rock'),
+VALUES('The Beatles', '1', 2000, 22.6, 'Rock'),
+VALUES('Michael Jackson', 'Dangerous', 1991, 16.3, 'Pop, Funk, Rock'),
+VALUES('Madonna', 'The Immaculate Collection', 1990, 19.4, 'Pop, Dance'),
+VALUES('The Beatles', 'Abbey Road', 1969, 14.4, 'Rock'),
+VALUES('Bruce Springsteen', 'Born in the U.S.A', 1984, 19.6, 'Rock'),
+VALUES('Dire Straits', 'Brothers in Arms', 1985, 17.7, 'Pop, Rock'),
+VALUES('James Horner', 'TitanicL Music from the Motion Picture', 1997, 18.1, 'Soundtrack'),
+VALUES('Metallica', 'Metallica', 1991, 21.2, 'Thrash Metal, Heavy Metal'),
+VALUES('Nirvana', 'Nevermind', 1991, 16.7, 'Grunge, Alternative Rock'),
+VALUES('Pink Floyd', 'The Wall', 1979, 17.6, 'Progressive Rock'),
+VALUES('Santana', 'Supernatural', 1999, 20.5, 'Rock'),
+VALUES('Guns N\' Roses', 'Appetite for Destruction', 1987, 21.6, 'Hard Rock');
+
+
+
+
+
+
